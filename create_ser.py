c0c679fee59ee4b45b851326e42758f337cb038e
Kyle-Ortiz <kyleaortiz@gmail.com>
2022-06-23 22:21:23 -0500

    added comments for clarity

--- create_ser.py
+++ create_ser.py
@@ -1,6 +1,6 @@
 import pandas as pd
 #import for pandas library giving it alias of pd
 
-ser1 = pd.Series()
+ser1 = pd.Series() #pandas method for creating a series, float datatype by default
 
-print(ser1)
+print(ser1) #printing new empty series to console
